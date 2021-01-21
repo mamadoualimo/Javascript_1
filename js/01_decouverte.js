cd8b610007cb7ed7a689275ca2bd32e39e556064
mamadoualimo <58432140+mamadoualimo@users.noreply.github.com>
2021-01-21 14:22:14 +0100

    presentation du langage

---
+++ js/01_decouverte.js
@@ -0,0 +1,1 @@
+//alert("Bonjour je suis le ficher javascript");
---
+++ js/02_variables.js
@@ -0,0 +1,16 @@
+// Declaration + affectation
+let prenom = "Alimou"; //String
+
+let age = 25; //Number
+
+let enLigne = true; //Boolean
+
+let choix = null; //Null
+
+console.log(typeof prenom);
+console.log(typeof age);
+console.log(typeof enLigne);
+console.log(typeof choix);
+
+//Concatenation
+console.log(prenom + ' ' + age + 'ans');
