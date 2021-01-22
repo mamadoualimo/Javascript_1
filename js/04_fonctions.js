a589b071786bafbbbb6b87cb69ac233341015922
mamadoualimo <58432140+mamadoualimo@users.noreply.github.com>
2021-01-22 09:31:04 +0100

    les fonctions

---
+++ js/04_fonctions.js
@@ -0,0 +1,12 @@
+// Creation d'une fonction avec parametre optionnel
+/*const message = function() {
+    let texte = 'Bonjour';
+    alert(texte);
+};
+message();*/
+
+//Creation d'une fonction flechée
+const somme = (a, b) => {
+    alert('resultat : ' + (a +b));
+};
+somme(10, 2);
